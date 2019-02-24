6ad17c1348b6a0ce03a07c2cfcd90312c1673998
Admir Kadriu <admirkadriu@hotmail.com>
2019-02-24 12:32:39 +0100

    Added test for clearing old indexes(for now are failing)

--- tests/functional/feature.test.js
+++ tests/functional/feature.test.js
@@ -67,33 +67,56 @@ test('Should set the redis client', () => {
 });
 
 test('Should create two users', async () => {
-  const res1 = await userModel.set(user1);
-  expect(res1).toBe(true);
+  const [res1, res2] = await Promise.all([userModel.set(user1), userModel.set(user2)]);
 
-  const res2 = await userModel.set(user2);
+  expect(res1).toBe(true);
   expect(res2).toBe(true);
 });
 
 test('Should get users with ids', async () => {
-  const res1 = await userModel.get('21e332e1e21');
-  expect(res1).toEqual(user1);
+  const [res1, res2] = await Promise.all([userModel.get('21e332e1e21'), userModel.get('d982398j2398u')]);
 
-  const res2 = await userModel.get('d982398j2398u');
+  expect(res1).toEqual(user1);
   expect(res2).toEqual(user2);
 });
 
 test('Should get users with emails', async () => {
-  const res1 = await userModel.getByEmail('test1user@tests.com');
-  expect(res1).toEqual(user1);
+  const [res1, res2] = await Promise.all([
+    userModel.getByEmail('test1user@tests.com'),
+    userModel.getByEmail('test2user@tests.com'),
+  ]);
 
-  const res2 = await userModel.getByEmail('test2user@tests.com');
+  expect(res1).toEqual(user1);
   expect(res2).toEqual(user2);
 });
 
 test('Should get users with type 1', async () => {
   const users = await userModel.getByType(1);
+
   expect(users.sort((u1, u2) => u1.id > u2.id))
     .toEqual([user1, user2].sort((u1, u2) => u1.id > u2.id));
+});
+
+test('Should change users email and clear old index', async () => {
+  const user = await userModel.get('21e332e1e21');
+
+  const oldEmail = user.email;
+  const newEmail = 'test3user@test.com';
+  user.email = newEmail;
+
+  const setResult = await userModel.set(user);
+  expect(setResult).toBe(true);
+
+
+  const userByNewEmail = await userModel.getByEmail(newEmail);
+
+  expect(userByNewEmail).toEqual(user);
+
+
+
+  const userByOldEmail = await userModel.getByEmail(oldEmail);
+
+  expect(userByOldEmail).toEqual(null);
 });
 
 test('Should disconnect redis', () => {
